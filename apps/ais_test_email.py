547e882d4f276324cf7800b4648f7f8055964309
bf_opengradient <40048681+bfopengradient@users.noreply.github.com>
2021-10-28 17:21:08 +1100

    Update ais_test_email.py

--- apps/ais_test_email.py
+++ apps/ais_test_email.py
@@ -9,6 +9,7 @@ import pandas as pd
 import streamlit as st
 import torch
 from torch.utils.data import DataLoader
+import transformers
 from transformers import DistilBertTokenizerFast, DistilBertForSequenceClassification, Trainer, TrainingArguments
 transformers.logging.set_verbosity_error()
 
