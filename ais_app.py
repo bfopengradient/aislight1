8023b42d963cabbd6db61d2dc2d9b4177dbd33ae
bf_opengradient <40048681+bfopengradient@users.noreply.github.com>
2021-11-02 08:57:31 +1100

    Update ais_app.py

--- ais_app.py
+++ ais_app.py
@@ -6,8 +6,8 @@ import streamlit as st
 
 #Main page that calls the multiapp 
 
-#LOGO_IMAGE  and title positioning 
-NAME_IMAGE = "./apps/RGB_Secondary.png"
+#LOGO_IMAGE  and title positioning ,  put in your own logo here!!
+NAME_IMAGE = "Your logo goes here"
 logo = Image.open(NAME_IMAGE)
 st.image(logo, width=700) 
 col1, col2, col3 = st.beta_columns([2,2,1])
